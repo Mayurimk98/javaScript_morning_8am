e4dc443eccf6b9342c02a0027a4d200a029d6999
Mayurimk98 <mayurimkatwe@gmail.com>
2023-02-14 09:25:41 +0530

    day2

---
+++ day2.js
@@ -0,0 +1,130 @@
+
+//function => 
+
+// //function without para and without return type
+
+// function mul(){
+//     console.log(5*5)
+// }
+// mul()
+// mul()
+// mul()
+
+// //function with para and wothout return type
+// function mul1(x,y){
+//     console.log(x*y)
+// }
+// mul1(2,3)//6
+// mul1(4,6)
+// mul1(3,3)
+
+// //function with para and with return type
+// function mul3(a,b){
+//     return a*b
+// }
+// let y1=mul3(5,5)
+// console.log(y1)
+
+// console.log(y1+10)
+// console.log(y1*5)
+
+
+//==============================================
+
+//3 type
+//function declaration
+
+// function add(x, y) {
+//     return x + y
+// }
+// let result = add(2, 3)
+// console.log(result)
+
+
+// //function expression
+// let add1 = function (a, b) {
+//     return a + b
+// }
+// let result1 = add1(2, 3)
+// console.log(result1)
+
+//x+y =20
+//x=20-y
+
+//arrow function //ES6
+// let add2 = (a, b) => {
+//     return a + b
+// }
+// let result2 = add2(2, 3)
+// console.log(result2)
+
+//=====================================================
+
+//comparision operators  //boolean => true/ false
+//< , > ,<= , >=, ==, ===, !=, !==
+
+//entity < entity => booelan
+
+console.log(10 > 5) //true
+console.log(20 < 30) //true
+console.log(100 <= 100) //true
+console.log(50 <= 20)  //false
+console.log(12 >= 12) //true
+console.log(45 >= 34) //true
+console.log(20 == '20') //true
+
+console.log(20 === '20') //false
+
+console.log(12 != '12') //false
+
+console.log(20 !== '20') //true
+
+//==  => check the value not datatype
+//=== => check value as well as datatype
+
+
+
+
+
+// console.log(typeof 20) //number
+// console.log(typeof "20") //string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let a=100
+// console.log(a) //100
+
+// //datatype  (typeOf)
+
+// console.log(typeof a)  //number
+
+// let fname="mayuri"  //string
+// let fname1='mayuri'
+// let fname2=`mayuri`
+
+// console.log(typeof fname)
+
+// let isDrive=true  //boolean
+// console.log(typeof isDrive)
+
+// let isDrive2=false
+// console.log(typeof isDrive2)
+
+
+
