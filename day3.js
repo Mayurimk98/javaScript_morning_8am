5b416f676aaf0b220ab2f2b53b1f2b46aecf1abe
Mayurimk98 <mayurimkatwe@gmail.com>
2023-02-16 09:14:06 +0530

    day3

---
+++ day3.js
@@ -0,0 +1,96 @@
+
+//javascript => object
+
+//person
+//mayuri, akash, swapnil, gita
+
+//vehical
+//nano, audi, merceed
+
+//bank
+//hdfc, ICICI, kotak mahindra
+
+// let a = 10
+// console.log(typeof a) //number
+// let fn = "mayuri"
+// console.log(typeof fn) //striing
+// let s1 = true
+// console.log(typeof s1) //boolean
+
+
+//comparision operator
+
+//<, >, <=, >=, ==, !=, ===, !==
+
+//==  and != ===> value check
+
+//=== and !== => value nd datatype
+// console.log(12 == '12') //true
+
+// console.log(45!='45') //false
+
+// console.log(13==='13') //false
+
+// console.log(34!=='34')//true
+
+
+//logical operators
+//And &&  (multiplication)
+
+//true && true => true
+//true && false => false 
+//false && true  => false
+//false && false => false
+
+// console.log(7==7 && 8!=='8') //true
+// //true && true =>true
+
+// console.log(12=='12' && 34!='34') //false
+// // true && false => false 
+
+// console.log(45==='45' && 34<=100) //false
+// //false && true => false
+
+// console.log(-3>0 && 12>=35) //false
+// // false && false => false 
+
+//OR || (addition)
+// true || true ==> true
+// true || false ==>true
+// false || true => true
+// false || false => false 
+
+// console.log(7==7 ||8!=='8')//true
+// //true || true =>true
+
+// console.log(37==37 || 45!='45')
+// //true || false  => true
+
+// console.log(56==='56' || 34=='34')
+// //false  || true => true
+
+// console.log(12==='12' || 10!='10')
+// //false || false => false 
+
+//not !
+//true =>!true ==> false
+// false => !false => true
+
+console.log(!(10=='10'))
+
+console.log(!(12==='12'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
