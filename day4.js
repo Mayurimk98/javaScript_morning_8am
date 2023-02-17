4ee31c37bbeed4c4d16ae245def1796053656357
Mayurimk98 <mayurimkatwe@gmail.com>
2023-02-17 09:14:02 +0530

    17 feb

---
+++ day4.js
@@ -0,0 +1,104 @@
+//logical operator
+
+//AND
+//OR
+//NOT
+
+//===============================
+
+//conditional statements
+
+//multiple condition
+
+//input => 
+
+//if / if -else / if else if /switch case
+
+//if syntax
+
+// if(true){
+// //statement
+// }
+
+
+// if (100 > 10) {
+//     console.log('100 is greter')
+// }
+
+// if(2==='2'){
+//     console.log('compare')
+// }
+// else{
+//   console.log('condition false')  
+// }
+
+//if-else
+// if(10<2){
+//     console.log('greter number')
+// }
+// else{
+//     console.log('smaller number')
+// }
+
+////================================================
+//multiple condition check with if 
+
+//progarm 1
+
+//ticket
+//1-5 ===> 10 % discount
+//6-10 ===> 20 % discount
+//>10  ===> 30 % discount
+
+// let ticketNumber = 2
+
+// if(ticketNumber>0 && ticketNumber<=5){  //2>0 && 2<=5  =>true
+//     console.log('You got 10 % discount')
+// }
+// if(ticketNumber>5 && ticketNumber<=10){ //2>5 && 2<=10 => false
+//     console.log('You got 20 % discount')
+// }
+// if(ticketNumber>10){ //2>10 //false
+//     console.log('You got 30 % discount')
+// }
+
+
+//problem 2
+
+
+// let ticketNumber = 6
+
+// if(ticketNumber>0 && ticketNumber<=5){    //6>0 && 6<=5 //false
+//     console.log('You got 10 % discount')
+// }
+// else if(ticketNumber>5 && ticketNumber<=10){  //6>5 && 6<=10  //true
+//     console.log('You got 20 % discount')
+// }
+// else if(ticketNumber>10){ 
+//     console.log('You got 30 % discount')
+// }
+
+//program
+// let marks = 90
+// if (marks > 80) { //90>80
+//     console.log('A gread')
+// }
+// if (marks > 60) { //90>60
+//     console.log('B gread')
+// }
+// if (marks > 40) { //90>40
+//     console.log('C gread')
+// }
+
+
+let marks = 50
+if (marks > 80) { //90>80
+    console.log('A gread')
+}
+else if (marks > 60) { 
+    console.log('B gread')
+}
+else if (marks > 40) {
+    console.log('C gread')
+}
+
