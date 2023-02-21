d85d1ae1af4ce54a44c24d5181a5863e4aa83571
Mayurimk98 <mayurimkatwe@gmail.com>
2023-02-21 08:37:28 +0530

    21 feb

---
+++ day6.js
@@ -0,0 +1,115 @@
+//switch case
+
+//withput break
+
+// let city = "Ujjain"
+// switch (city) {
+//     case "Nashik":
+//         console.log('MH')
+//     case "Ujjain":
+//         console.log('MP')
+//     case "Jaipur":
+//         console.log('RJ')
+// }
+
+// let city = "Ujjain"
+// switch (city) {
+//     case "Nashik":
+//         console.log('MH')
+//         break;
+//     case "Ujjain":
+//         console.log('MP')
+//         break;
+//     case "Jaipur":
+//         console.log('RJ')
+//         break;
+// }
+
+
+// let city = "Pune"
+// switch (city) {
+//     case "Nashik":
+//         console.log('MH')
+//         break;
+//     case "Ujjain":
+//         console.log('MP')
+//         break;
+//     case "Jaipur":
+//         console.log('RJ')
+//         break;
+//     default:
+//         console.log('Invalid city')
+// }
+
+// let city = "Indore"
+// switch (city) {
+//     case "Nashik":
+//     case "Pune":
+//         console.log('MH')
+//         break;
+//     case "Ujjain":
+//     case "Indore":
+//         console.log('MP')
+//         break;
+//     case "Jaipur":
+//     case "Rajsthan":
+//         console.log('RJ')
+//         break;
+//     default:
+//         console.log('Invalid city')
+// }
+
+// let Country ="Spain"
+// switch (Country) {
+//     case "France":
+//     case "Spain":
+//     case "Netherland":
+//     case "Italy":
+//         console.log('This Country is in Europe')
+//         break
+//     case "United State":
+//     default:
+//         console.log('This country is not in Europe')
+// }
+
+//conditional
+
+//if
+//if else
+//else if
+//switch case
+//ternary operator
+
+
+//==================================================================
+
+//1-5 print
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+//1-100
+
+//1-10000  ==> time waste, effort, memory
+
+//loops  => repatative based on condition
+//for loop
+//while loop
+//do while loop
+
+
+//Syntax for loop
+// for(initialised;condition check;increment/decrement){
+//     //statemenet
+// }
+
+//print 1-5
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+
+
+
