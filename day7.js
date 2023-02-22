d54ebac3ea095e53326730a6da9259ebdcabf743
Mayurimk98 <mayurimkatwe@gmail.com>
2023-02-22 08:32:07 +0530

    22 feb

---
+++ day7.js
@@ -0,0 +1,89 @@
+
+
+//loop 
+
+//for loop
+
+// for(initialization;conditioncheck;incre/decre){
+//     //statements
+// }
+
+
+//0-5 (forward loop)
+// for (let i = 0; i <= 5; i++) { //1 2 3 4  5  6
+//     console.log(i)  //0  1  2  3  4  5
+// }
+
+//0<=5  ==> T
+//1<=5 ==> T
+//2<=5 ==> T
+//3<=5  ==> T
+//4<=5 ==> T
+//5<=5 ==> T
+//6<=5  ==> F
+
+///=======================================================
+//print hello 10 time
+
+// for (let i = 0; i < 10; i++) {
+//     //console.log(i)
+//     console.log('hello')
+// }
+
+//=======================================================
+///5-1(reverse loop)
+
+// for (let i = 5; i >= 1; i--) { //4  3  2 1  0
+//     console.log(i) //5 4 3 2 1
+// }
+
+//5>=1  ==> T
+//4>=1  ==> T
+//3>=1 ==> T
+//2>=1  ==> T
+//1>=1  ==> T
+//0>=1 ==> F
+
+//====================================================
+
+//while  ==> duration dont know
+
+//for loop ==> duration known
+
+//SYNTAX
+
+//initialisation
+//while(condition){
+//statemnets
+//incre/decre
+//}
+
+
+//1-5
+let i = 1 //i=1
+while (i <= 5) { //1<=5  2<=5  3<=5 4<=5  5<=5  6<=5
+    console.log(i) //1 2  3  4  5
+    i++ //2 3  4  5  6
+}
+
+//10-1
+let j = 10
+while (j >= 1) {
+    console.log(j)
+    j--
+}
+
+
+
+
+
+
+
+
+
+// let fn="mayuri";
+// let fn=10
+
+
+
+
