01c8e8a3c33f95ef75c29e66834d725aaf4efea2
Mayurimk98 <mayurimkatwe@gmail.com>
2023-03-02 08:33:08 +0530

    3 march

---
+++ day10.js
@@ -0,0 +1,55 @@
+
+//Math.floor()
+
+// console.log(Math.floor(12.5)) //12
+// console.log(Math.floor(63.5)) //63
+// console.log(Math.floor(-45.67)) //-46
+// console.log(Math.floor(0.75)) //0
+
+// -46   -45.67   -45      -2  -1  0 0.75   1   2   3  4   .......  12  12.5  12.75  13   13.5
+
+
+//==============================================================
+
+//Math.ceil()
+// console.log(Math.ceil(45.9)) //46
+// console.log(Math.ceil(8.23)) //9
+// console.log(Math.ceil(-13.78)) //-13
+
+//==============================================================
+
+//Math.round(12.67) //13
+//Math.round(10.23) //10
+
+// console.log(Math.round(8.45)) // 8
+// console.log(Math.round(56.98)) //57
+// console.log(Math.round(23)) //23
+// console.log(Math.round(-34.56)) //-35
+
+//========================================================
+//Math.floor(number)
+//Math.ceil(number)
+//Math.round(number)
+
+
+//console.log(Math.random())//0.0-0.99
+
+//1-5 random
+
+//console.log(Math.random()*5) //0.0-4.95
+
+//console.log(Math.floor(Math.random()*5)) //0-4
+// Math.floor(2.9599422358939176) //2
+
+console.log(Math.floor(Math.random()*5)+1) //1-5
+
+// guessing game 
+
+//1-5  => system
+//enter no ==> 1-5 ==> user
+
+//system no==user no ==> correct
+
+
+
+
