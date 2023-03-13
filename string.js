39fc4d840ebc0625eaab1f9c794e0c9f21dcb5a6
Mayurimk98 <mayurimkatwe@gmail.com>
2023-03-13 09:01:34 +0530

    string

---
+++ string.js
@@ -0,0 +1,236 @@
+
+
+// //String
+
+// //javascript
+// //function
+// //arithmatic operators
+// //comparision
+// //logical 
+// //conditional statement
+// //loop
+// //guess input
+// //Array
+
+// //String
+// //javascript==> Object
+// // //property and methods
+
+// // let firstName = "Mayuri"
+// // console.log(firstName)//Mayuri
+// // console.log(typeof firstName)
+
+// // let lastName = 'katwe'
+// // console.log(typeof lastName)
+
+// // let city = `Nashik`
+// // console.log(typeof city)
+
+// // let aa='y'
+// // console.log(typeof aa)
+
+// // let state = "Maharashtrahhhhhhhhhhhhhhhtttttuueuueq"
+
+// // // M    a   h   a   r  a  s  h  t  r   a
+// // // 0    1   2   3   4  5  6  7  8  9   10
+
+// // //index==> 0
+
+// // // console.log(state[0]) //M
+
+// // // console.log(state[7]) //h
+
+// // // //length ==> property
+// // // console.log(state.length) //11
+
+// // //lastelement string
+
+// // // console.log(state[10]) //a
+
+// // console.log(state[state.length-1])
+
+
+// //Methods
+
+// //1. toUpperCase()
+// //action=> to convert all char into uppercase
+// //return => new string
+// // let city="Nashik"
+// // let y1=city.toUpperCase()
+// // console.log(y1) //"NASHIK"
+
+// //2. toLowerCase()
+// //action=> convert all char into small case/ lowercase
+// //return => new string
+
+// // let city="Nashik"
+// // let y2=city.toLowerCase()
+// // console.log(y2) //nashik
+
+// // let city1="PuNe"
+// // console.log(city1.toLowerCase())
+
+// //mayuri ==> mayuri
+// //MAYURI==> mayuri
+// //mAyUrI=> mayuri
+
+// //3. indexOf()
+// //action => check whether char present in string or not if present return index of not -1
+// //retun  => number
+
+// // let city2="Nashik"
+
+// // // N   a   s  h  i  k
+// // // 0   1   2  3  4  5
+
+// // let y3=city2.indexOf('n')
+// // console.log(y3) //-1
+
+// // let y4=city2.indexOf('i')
+// // console.log(y4)//4
+
+// //4.includes()
+// //action==> check whether char present in string or not if present true else false
+// //return ==> boolean
+
+// // let city2 = "Nashik"
+// // let y4 = city2.includes('H')
+// // console.log(y4)
+
+// // let y5=city2.includes('a')
+// // console.log(y5)
+
+// //5. startsWith()
+
+// //action  => check whether string starts with given char or substring if startwith true else false
+// //return => boolean
+
+// //subString==> ag, Nag, pur  ===> gru==> not allow
+
+// // let city="Nagpur"
+// // let m1=city.startsWith('N')
+// // console.log(m1)
+
+// // let m2=city.startsWith('g')
+// // console.log(m2)
+
+// // let m3=city.startsWith('Na')
+// // console.log(m3)
+
+// //6. endsWith()
+// //action  => check whether string ends with given char or substring if endwith true else false
+// //return => boolean
+
+// let firstName = "Mayuri"
+// let u1 = firstName.endsWith('ui')
+// console.log(u1)//false
+
+// let u2=firstName.endsWith('ri')
+// console.log(u2)
+
+// let u3=firstName.endsWith('i')
+// console.log(u3)
+
+//1. toUpperCase()
+//2. toLowerCase()
+//3. indexOf()
+//4. includes()
+//5. startWith()
+//6 . endsWith()
+
+
+//=================================================================
+
+// let a=10
+// let b=5
+
+// //addition 
+// //substraction
+
+// console.log(a+b)//15
+// console.log(a-b)//5
+
+// let y1=100
+// let y2=2
+
+// console.log(y1+y2) //101
+// console.log(y1-y2)//98
+
+// let s1=40
+// let s2=5
+// console.log(s1+s2)
+// console.log(s1-s2)
+
+
+//1 => 2 
+//10 => 20
+//100 => 200
+
+//DRY==> donot repeat yourself
+//function
+
+//function ==> keyword
+//add => function  name
+//()=> paranthesis
+//{} => block
+
+// function add(){
+// //statemenet
+// }
+
+// add() //calling the function
+
+// function add1(x,y){
+//    console.log(x+y) 
+//    console.log(x-y)
+// }
+// add1(10,5)
+// add1(100,2)
+// add1(40,5)
+
+//3 type
+
+//1. function without parameter and without return type
+
+// function sum(){
+//     console.log(10+2)
+// }
+// sum() //12
+// sum() //12
+// sum()//12
+
+//2. function with paramter and without return type
+
+// function sum1(a,b){
+//     console.log(a+b)
+// }
+// sum1(2,5) //7
+// sum1(10,2) //12
+// sum1(33,2) //35
+
+//3. function with para and with return type
+
+// function sum3(s,u){
+//     return s+u
+// }
+// let p1=sum3(12,3)
+// console.log(p1)//15
+// console.log(p1+10)//25
+// console.log(p1*5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
