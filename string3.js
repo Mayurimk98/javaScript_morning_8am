2ebd2ee0696ebe180da4fc9d3a008aa7fcb3a3f0
Mayurimk98 <mayurimkatwe@gmail.com>
2023-03-15 08:57:09 +0530

    15 march

---
+++ string3.js
@@ -0,0 +1,91 @@
+
+
+// let city="  Nagpur  "
+// console.log(typeof city)
+// console.log(city.length)
+// let y1=city.trim()
+// console.log(y1)
+// console.log(y1.length)
+
+//11.split() //array
+
+//split('a') //[m,yuri]
+
+// let firstName="Mayuri Mahendra Katwe"
+// // let y2=firstName.split('a')
+// // console.log(y2)
+
+// let y3=firstName.split(' ')
+// console.log(y3)
+
+// let y4=firstName.split("")
+// console.log(y4)
+
+
+// let s1="200$00"
+// let y5=s1.split('$')
+// console.log(y5) //[200,00]
+// console.log(y5[0])
+
+
+//12. charAt()
+
+//return th char of given index id index is invalid return blank string
+// let city = "Pune"
+// // P   u  n  e
+// // 0   1  2  3
+
+// let m1 = city.charAt(1)
+// console.log(m1)
+
+// let m2 = city.charAt(7)
+// console.log(m2) //blank string
+
+//13.charCodeAt()
+// //ASCII value
+
+// let city = "Pune"
+// // P   u  n  e
+// // 0   1  2  3
+
+// let u2=city.charCodeAt(2)
+// console.log(u2)
+
+// let u3=city.charCodeAt(0)
+// console.log(u3)
+
+// let u4=city.charCodeAt(10)
+// console.log(u4)//NaN
+//================================================================
+
+// let student=["mayuri","vaibhav"]
+// let u11=student.join("-").toUpperCase().split("-").indexOf('VAIBHAV')//.includes('m')
+// console.log(u11)
+// //mayuri-vaibhav
+// //MAYURI-VAIBHAV
+// //[MAYURI,VAIBHAV]
+
+//===============================================================================================
+
+//STRING INTERPOLATION
+
+// let firstName="mayuri"
+// console.log(firstName) //mayuri
+// let lastName="Katwe"
+// console.log(lastName) //katwe
+
+// //My firstName is mayuri and my lastName is katwe
+
+// //console.log("My firstName is"+firstName+"and my lastName is"+lastName)
+// //My firstName ismayuriand my lastName isKatwe
+
+// //console.log("My firstName is"+" "+firstName+" "+"and my lastName is"+" "+lastName)
+
+// console.log(`My firstName is ${firstName}  and my lastName is ${lastName}`)
+
+//string+string => string
+//string+number=>string
+//number+string==> string
+//number+number=> number
+
+
