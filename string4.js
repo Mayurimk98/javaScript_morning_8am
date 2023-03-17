9ac1bc7eb36b524a00e1442c54e6071a82238d49
Mayurimk98 <mayurimkatwe@gmail.com>
2023-03-17 08:45:09 +0530

    string 17

--- string4.js
+++ string4.js
@@ -1,115 +1,115 @@
 
-//1. toUpperCase()
-//2. toLowerCase()
-//3. trim()
-//4. trimStart()
-//5. trimEnd()
-//6. startWith()
-//7. endsWith()
-//8. indexOf()//
-//9. includes()
-//10. slice()
+// //1. toUpperCase()
+// //2. toLowerCase()
+// //3. trim()
+// //4. trimStart()
+// //5. trimEnd()
+// //6. startWith()
+// //7. endsWith()
+// //8. indexOf()//
+// //9. includes()
+// //10. slice()
 
 
-// let city="Nashik"
-// //-6   -5  -4   -3  -2  -1 
-// //N     a   s   h   i   k
-// //0     1   2   3   4   5
+// // let city="Nashik"
+// // //-6   -5  -4   -3  -2  -1 
+// // //N     a   s   h   i   k
+// // //0     1   2   3   4   5
 
-// let y1=city.slice(-6)
-// console.log(y1) //
+// // let y1=city.slice(-6)
+// // console.log(y1) //
 
-// let y2=city.slice(4,-6)
-// console.log(y2)//
+// // let y2=city.slice(4,-6)
+// // console.log(y2)//
 
-// let y3=city.slice(-2,-1)
-// console.log(y3)//i
+// // let y3=city.slice(-2,-1)
+// // console.log(y3)//i
 
-//11. split()//string
-//action=> break the string on given char,special char space
-//return ==> array
+// //11. split()//string
+// //action=> break the string on given char,special char space
+// //return ==> array
 
-// let name1="mayuri mahendra katwe"
-// let u1=name1.split(" ")
-// console.log(u1) //["mayuri","mahendra","katwe"]
+// // let name1="mayuri mahendra katwe"
+// // let u1=name1.split(" ")
+// // console.log(u1) //["mayuri","mahendra","katwe"]
 
-// let currency="200$000"
-// let u2=currency.split('$')
-// console.log(u2) //["200","000"]
-// console.log(u2[0])
+// // let currency="200$000"
+// // let u2=currency.split('$')
+// // console.log(u2) //["200","000"]
+// // console.log(u2[0])
 
-// let firstName="Mayuri"
-// let u3=firstName.split('y')
-// console.log(u3) //["Ma","uri"]
+// // let firstName="Mayuri"
+// // let u3=firstName.split('y')
+// // console.log(u3) //["Ma","uri"]
 
-// //Mauri
-// let u5=u3.join("")
-// console.log(u5)
+// // //Mauri
+// // let u5=u3.join("")
+// // console.log(u5)
 
 
-// let num="1000&200"
-// //1000-200
-// console.log(num.split("&").join("-"))
-// //["1000","200"]
+// // let num="1000&200"
+// // //1000-200
+// // console.log(num.split("&").join("-"))
+// // //["1000","200"]
 
-//12. charAt()
-//action==> give the char of given index
-//return => string
+// //12. charAt()
+// //action==> give the char of given index
+// //return => string
 
-// let fruit="banana"
+// // let fruit="banana"
 
-// // b    a   n   a   n    a
-// // 0    1   2   3   4   5
-// let y8=fruit.charAt(2)
-// console.log(y8) //n
+// // // b    a   n   a   n    a
+// // // 0    1   2   3   4   5
+// // let y8=fruit.charAt(2)
+// // console.log(y8) //n
 
 
-// let y9=fruit.charAt(5)
-// console.log(y9) //a
+// // let y9=fruit.charAt(5)
+// // console.log(y9) //a
 
-// let y10=fruit.charAt(9)
-// console.log(y10)//blank string
+// // let y10=fruit.charAt(9)
+// // console.log(y10)//blank string
 
-//13. charCodeAt()
-//action => return the ascii value of char of given index
-//return =>number
+// //13. charCodeAt()
+// //action => return the ascii value of char of given index
+// //return =>number
 
 
-let fruit = "Banana"
+// let fruit = "Banana"
 
-// // B    a   n   a   n    a
-// // 0    1   2   3   4   5
+// // // B    a   n   a   n    a
+// // // 0    1   2   3   4   5
 
-let m2=fruit.charCodeAt(3)
-console.log(m2)
+// let m2=fruit.charCodeAt(3)
+// console.log(m2)
 
-let m3=fruit.charCodeAt(0)
-console.log(m3)
+// let m3=fruit.charCodeAt(0)
+// console.log(m3)
 
 
-//string concatination
+// //string concatination
 
-//+
+// //+
 
-//string+string==> string
-//string+number=> string
-//number+string=> string
-//number+number=> number
+// //string+string==> string
+// //string+number=> string
+// //number+string=> string
+// //number+number=> number
 
-let a="mayuri"
-let b="shamal"
-let c=20
-let d=40
+// let a="mayuri"
+// let b="shamal"
+// let c=20
+// let d=40
 
-//left => right
-console.log(c+d+a) //60mayuri
-//20+40=> 60+"mayuri"
-console.log(b+c+d) //shamal2040
-//shamal+20  => shamal20+40
+// //left => right
+// console.log(c+d+a) //60mayuri
+// //20+40=> 60+"mayuri"
+// console.log(b+c+d) //shamal2040
+// //shamal+20  => shamal20+40
 
-console.log(a+b+d+c)//mayurishamal4020
+// console.log(a+b+d+c)//mayurishamal4020
 
-console.log(c+d)//numner
+// console.log(c+d)//numner
 
 
 
@@ -133,52 +133,11 @@ console.log(c+d)//numner
 
 
 
-//at()//undefined
-// let num=[10,20,30]
+// //at()//undefined
+// // let num=[10,20,30]
 
-// let m1=num.at(5)
-// console.log(m1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let num=[1,2,3,4]
-// let y22=num.join("")
-// console.log(y22)//string
-
-
+// // let m1=num.at(5)
+// // console.log(m1)
 
 
 
---
+++ string5.js
@@ -0,0 +1,112 @@
+
+//string concatination(+)
+// console.log("20+20") //20+20
+
+
+// //String+string=> String
+// //String+Number==> String
+// //Number+String==> String
+// //Number+Number==>Number
+
+// let a=10
+// let b="Mayuri"
+// let c=30
+// console.log(a+c) //40
+// console.log(a+c+b) //40Mayuri
+// console.log(b+c+a) //Mayuri3010
+
+
+// let firstName="Vaibhav"
+// let lastName="Katwe"
+
+//My firstName is Vaibhav and My lastName is Katwe
+
+//console.log("My firstName is"+firstName+"and My lastName is"+lastName)
+
+// console.log("My firstName is"+" "+firstName+" "+"and My lastName is"+" "+lastName)
+//My firstName isVaibhavand My lastName isKatwe
+
+//String interpolation
+// console.log(`My firstName is ${firstName} and my lastName is ${lastName}`)
+
+// let p1=(`${5+5}`)
+// console.log(p1)
+// console.log(typeof p1)
+
+// console.log(5+5)
+
+//========================================================
+
+// let fn = "MayuriK"
+// for(let i=0;i<fn.length;i++){
+//     //console.log(i)
+//     console.log(fn[i])
+// }
+
+//=====================================================
+// for(let i=fn.length-1;i>=0;i--){
+//     console.log(fn[i])
+// }
+//====================================================
+
+// let fn = "MayuriK"
+// let rav = "" //aM ==> yaM => uyaM => ruyaM => KiruyaM
+// for (let i = 0; i < fn.length; i++) {
+//     //console.log(fn[i])
+//     rev = fn[i] + rev //r+uyaM
+// }
+//=============================================================
+
+let fn = "Mayu"
+//"uyaM"
+
+// let rev = "" //M =>aM=>yaM =>uyaM
+// for (let i = 0; i < fn.length; i++) {
+//     //console.log(fn[i])
+//     rev = fn[i] + rev
+// }
+// console.log(rev)
+
+//M+"" =>M
+//a+M=>aM
+//y+am=>yaM
+//u+yaM=>uyaM
+
+//================================================
+
+// let forw = ""
+// for (let i = 0; i < fn.length; i++) {
+//     forw = forw + fn[i]
+// }
+// console.log(forw) //Mayu
+
+// //Mayu
+
+//================================================
+
+let city = "Pune"
+//enuP
+// let rev=""
+// for(let i=city.length-1;i>=0;i--){
+//     //console.log(city[i])
+//     //rev=city[i]+rev //Pune
+//     rev=rev+city[i] //enuP
+// }
+// console.log(rev)
+
+
+// let y1 = city.split("").reverse().join("")
+// console.log(y1)
+
+//['p','u','n','e']
+//[e,n,u,P]
+//enuP
+
+//==================================
+let firstName="MayuriRoa"
+
+//a e i o u
+//count =>5
+
+
+
