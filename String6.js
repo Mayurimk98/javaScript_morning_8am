c32f1f85258eff3690dbf10a521dc67a7c24bb2c
Mayurimk98 <mayurimkatwe@gmail.com>
2023-03-18 12:23:46 +0530

    string revision

---
+++ String6.js
@@ -0,0 +1,113 @@
+
+
+// let city="Nashik"
+// for(let i=0;i<city.length;i++){
+//     //console.log(i)
+//     console.log(city[i])
+// }
+
+//=======================================
+
+// for(let i=city.length-1;i>=0;i--){
+//     console.log(city[i])
+// }
+
+//===================================
+let city = "Nashik"
+//kihsaN
+// let rev = "" //N => aN =>saN=>hsaN => kihsaN
+// for (let i = 0; i < city.length; i++) {
+//     rev = city[i] + rev
+// }
+// console.log(rev)
+
+//N =>N
+//a+N=>aN
+//s+aN=> saN
+//h+saN=> hsaN
+
+// let aa = ""
+// for (let i = city.length - 1; i >= 0; i--) {
+//     //aa = aa + city[i] //kihsaN
+//     aa = city[i] + aa//Nashik
+// }
+// console.log(aa)
+//k =>k
+//k+i=> ki
+//ki+h=>  kih
+//kih+s=> kihs
+//kihs+a=> kihsa
+//kihsa+N=> kihsaN
+
+
+// let firstName = "mayurimkmaui"
+// let count = 0
+// for (let i = 0; i < firstName.length; i++) {
+//     if (firstName[i] == 'm') {
+//         count = count + 1
+//     }
+// }
+// console.log(count)
+
+
+//======================================
+
+//string interpolation
+let firstName="mayuri"
+let lastName="katwe"
+//My name is mayuri and my lastname is katwe
+
+
+//string concatination(+)
+
+//string+string=> string
+//string+number=> string
+//number+string=>string
+//number+number=> number
+
+
+
+
+
+
+
+
+
+
+
+// let arr=[1,2,3]
+
+// let sum=0
+// for(let i=0;i<arr.length;i++){
+//     sum=sum+arr[i]
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1-5
+// for(let i=1;i<=5;i++){
+//     console.log(i)
+// }
