28d1615c82f505541dd2bb2494c839dab75c10b3
Mayurimk98 <mayurimkatwe@gmail.com>
2023-04-06 08:40:13 +0530

    dom day 2

---
+++ DOM2/script.js
@@ -0,0 +1,14 @@
+//User interaction 
+
+//<h6>Amar</h6>
+
+//Global object
+
+//window  => parent
+//document
+
+document.querySelector("h1").addEventListener("click",function(){
+    document.querySelector('h1').style.color="blue"
+    document.querySelector('h1').style.backgroundColor="yellow"
+})
+
