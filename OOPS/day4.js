835443dcaf347b9ef6cd6d5ae2183276e278f62c
Mayurimk98 <mayurimkatwe@gmail.com>
2023-05-02 10:15:06 +0530

    day4 inheritance

--- OOPS/day4.js
+++ OOPS/day4.js
@@ -35,12 +35,18 @@
 //         console.log(`I have ${this.car}`)
 //     }
 // }
+
+// let shantanu=new GrandFather()
+// console.log(shantanu)
 // let Waman=new GrandFather()
 // console.log(Waman)
 // Waman.displayMoney()//2000
 // //Waman.displayCar()
 // console.log(Waman.surname)
 // console.log(Waman.money)
+
+// Waman.money=5000;
+// console.log(Waman)
 
 
 // let Mahendra =new Father()
@@ -61,21 +67,106 @@
 //========================================================================================
 
 //Parent having constructor and child dont have constructor
-class GrandFather{
-    constructor(surname, money){
-        this.surname=surname
-        this.money=money
-    }
-    displayMoney(){
-        console.log(this.money)
+// class GrandFather{
+//     constructor(surname, money){
+//         this.surname=surname
+//         this.money=money
+//     }
+//     displayMoney(){
+//         console.log(this.money)
+//     }
+// }
+
+// class Father extends GrandFather{
+//     home="shreeNiwas"
+//         displayHome(){
+//             console.log(`I have my own house name as ${this.home}`)
+//         } 
+// }
+
+// let Santosh=new GrandFather("shinde",200)
+// console.log(Santosh.surname)
+// console.log(Santosh.money)
+// //console.log(Santosh.home)//error
+// Santosh.displayMoney()
+// //Santosh.displayHome()
+
+// let Ganesh=new Father("Raut",8000)
+// console.log(Ganesh)
+// console.log(Ganesh.surname, Ganesh.money, Ganesh.home)
+// Ganesh.displayHome()
+// Ganesh.displayMoney()
+
+//==========================================================================
+
+//Parent having constructor and child also have constructor
+
+// class GrandFather{
+//     constructor(surname, money){
+//         this.surname=surname
+//         this.money=money
+//     }
+//     displayMoney(){
+//         console.log(this.money)
+//     }
+// }
+
+// class Father extends GrandFather{
+//     constructor(surname,money,home){
+//         super(surname,money)
+//         this.home=home
+//     }
+//     displayHome(){
+//         console.log(`Home name ${this.home}`)
+//     }
+// }
+
+// let ankush=new Father("Kute",7000,"shreesVaibhav")
+// console.log(ankush)
+
+// let ram=new GrandFather("malpathak",9000)
+// console.log(ram)
+
+
+// let mayuri={}
+
+// mayuri.fullName="mayuriKatwe"
+
+
+//single level
+//multi level
+//multiple
+
+
+//======================================================================
+
+
+//texing()  ==> 1 feature
+//multimedia()  => texing()+multimedia()
+//calling() => texing () , mulimedia(), calling()
+
+
+class WhatsApp1{
+    texing(){
+        console.log('I am texing feature')
     }
 }
 
-class Father{
-    home="shreeNiwas"
-        displayHome(){
-            console.log(`I have my own house name as ${this.home}`)
-        } 
+class WhatsApp2 extends WhatsApp1{
+    multiMedia(){
+        console.log('I am multimedia feature')
+    }
 }
 
-let Santosh=new Father()
+class WhatsApp3 extends WhatsApp2{
+    calling(){
+        console.log('I am calling feature')
+    }
+}
+
+let testWhatsApp=new WhatsApp3()
+console.log(testWhatsApp)
+
+testWhatsApp.texing()
+testWhatsApp.multiMedia()
+testWhatsApp.calling()
